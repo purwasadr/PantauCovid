66c49b4ccfc7b0319a6f16f58e30d01a510f8d3a
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 16:07:39 +0700

    Memperbaiki query yang salah

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/network/HospitalService.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/network/HospitalService.kt
@@ -3,6 +3,7 @@ package com.purwasadr.pantaucovid.data.source.remote.network
 import com.purwasadr.pantaucovid.BuildConfig
 import com.purwasadr.pantaucovid.data.source.remote.response.CityResponse
 import com.purwasadr.pantaucovid.data.source.remote.response.CovidResponse
+import com.purwasadr.pantaucovid.data.source.remote.response.HospitalResponse
 import com.purwasadr.pantaucovid.data.source.remote.response.ProvinceResponse
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -20,6 +21,15 @@ interface HospitalService {
     suspend fun getCities(
         @Query("provinceid") provinceId: String
     ): CityResponse
+
+    @GET("api/get-hospitals?")
+    suspend fun getHospitals(
+        @Query("provinceid") provinceId: String,
+        @Query(value ="cityid", encoded = false)
+        propAndCity: String,
+        @Query("type") type: String = "1"
+    ): HospitalResponse
+
     companion object {
         fun create(): HospitalService {
             val client = OkHttpClient.Builder()
