c0a63fd614e90991c9cead46225a51d966430bba
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 14:35:29 +0700

    Memberi extension function

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/CovidResponse.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/CovidResponse.kt
@@ -1,6 +1,7 @@
 package com.purwasadr.pantaucovid.data.source.remote.response
 
 import com.google.gson.annotations.SerializedName
+import com.purwasadr.pantaucovid.data.source.local.entity.CovidEntity
 
 data class CovidResponse(
     @field:SerializedName("data")
@@ -9,3 +10,13 @@ data class CovidResponse(
     @field:SerializedName("update")
     val update: UpdateResponse? = null
 )
+
+fun CovidResponse.toEntity() =
+    CovidEntity(
+        jumlahPositif = this.update?.total?.jumlahPositif,
+        jumlahSembuh = this.update?.total?.jumlahSembuh,
+        jumlahMeninggal = this.update?.total?.jumlahMeninggal,
+        jumlahOdp = this.data?.jumlahOdp,
+        jumlahPdp = this.data?.jumlahPdp,
+        id = 1
+    )
