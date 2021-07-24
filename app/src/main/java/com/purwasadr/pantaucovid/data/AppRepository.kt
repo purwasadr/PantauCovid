8eeb98960e31278858d3980d3d8e851c7db5d381
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 14:32:29 +0700

    Menambah method untuk HospitalService

--- app/src/main/java/com/purwasadr/pantaucovid/data/AppRepository.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/AppRepository.kt
@@ -1,24 +1,24 @@
 package com.purwasadr.pantaucovid.data
 
 import com.purwasadr.pantaucovid.data.source.local.ILocalDataSource
+import com.purwasadr.pantaucovid.data.source.local.entity.CityEntity
 import com.purwasadr.pantaucovid.data.source.local.entity.toDomain
 import com.purwasadr.pantaucovid.data.source.remote.IRemoteDataSource
 import com.purwasadr.pantaucovid.data.source.remote.network.ApiResponse
-import com.purwasadr.pantaucovid.data.source.remote.response.CovidDataResponse
+import com.purwasadr.pantaucovid.data.source.remote.response.CityResponse
 import com.purwasadr.pantaucovid.data.source.remote.response.CovidResponse
+import com.purwasadr.pantaucovid.data.source.remote.response.ProvinceResponse
 import com.purwasadr.pantaucovid.data.source.remote.response.toEntity
 import com.purwasadr.pantaucovid.model.Covid
+import com.purwasadr.pantaucovid.model.Province
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import okhttp3.internal.notify
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class AppRepository @Inject constructor(
     private val remoteDataSource: IRemoteDataSource,
     private val localDataSource: ILocalDataSource
-) : IAppRepository{
+) : IAppRepository {
 
     override fun getCovidData() = object : NetworkBoundResource<Covid?, CovidResponse>() {
         override fun loadFromDB(): Flow<Covid?> =
@@ -29,7 +29,7 @@ class AppRepository @Inject constructor(
         override fun shouldFetch(): Boolean = true
 
         override suspend fun createCall(): Flow<ApiResponse<CovidResponse>> =
-           remoteDataSource.getCovidData()
+            remoteDataSource.getCovidData()
 
 
         override suspend fun saveCallResult(data: CovidResponse) {
@@ -37,4 +37,41 @@ class AppRepository @Inject constructor(
         }
 
     }.asFlow()
+
+    override fun getProvinceList(): Flow<Resource<List<Province>>> =
+        object : NetworkBoundResource<List<Province>, ProvinceResponse>() {
+            override fun loadFromDB(): Flow<List<Province>> =
+                localDataSource.getProvinceList().map {
+                    it.toDomain()
+                }
+
+            override fun shouldFetch(): Boolean = true
+
+            override suspend fun createCall(): Flow<ApiResponse<ProvinceResponse>> =
+                remoteDataSource.getProvince()
+
+            override suspend fun saveCallResult(data: ProvinceResponse) {
+                data.toEntity()?.also {
+                    localDataSource.insertProvince(it)
+                }
+            }
+        }.asFlow()
+
+    override fun getCities(id: String): Flow<Resource<List<CityEntity>>> =
+        object : NetworkBoundResource<List<CityEntity>, CityResponse>() {
+            override fun loadFromDB(): Flow<List<CityEntity>> =
+                localDataSource.getCities()
+
+            override fun shouldFetch(): Boolean = true
+
+            override suspend fun createCall(): Flow<ApiResponse<CityResponse>> =
+                remoteDataSource.getCities(id)
+
+            override suspend fun saveCallResult(data: CityResponse) {
+                data.toEntity()?.also {
+                    localDataSource.insertCities(it)
+                }
+            }
+        }.asFlow()
+
 }
--- app/src/main/java/com/purwasadr/pantaucovid/data/IAppRepository.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/IAppRepository.kt
@@ -1,9 +1,14 @@
 package com.purwasadr.pantaucovid.data
 
+import com.purwasadr.pantaucovid.data.source.local.entity.CityEntity
 import com.purwasadr.pantaucovid.model.Covid
+import com.purwasadr.pantaucovid.model.Province
 import kotlinx.coroutines.flow.Flow
 
 interface IAppRepository {
     fun getCovidData(): Flow<Resource<Covid?>>
 
+    fun getProvinceList(): Flow<Resource<List<Province>>>
+
+    fun getCities(id: String): Flow<Resource<List<CityEntity>>>
 }
