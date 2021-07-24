f16e58a8b4f19c0a4190e0284263c232b876d010
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 16:09:59 +0700

    Menambah fungsi yang diperlukan

--- app/src/main/java/com/purwasadr/pantaucovid/ui/main/MainActivity.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/ui/main/MainActivity.kt
@@ -2,15 +2,73 @@ package com.purwasadr.pantaucovid.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.purwasadr.pantaucovid.R
 import com.purwasadr.pantaucovid.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupToolbar()
+        setupBottomNavigation(savedInstanceState)
+    }
+
+    private fun setupBottomNavigation(savedInstanceState: Bundle?) {
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.homeFragment, R.id.homeFragment),
+        )
+
+        binding.bottomNavMain.setupWithNavController(navController)
+    }
+
+//    private fun setupOnDestinationChange() {
+//        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+//            supportActionBar?.also {
+//                it.show()
+//            }
+//        }
+//    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            title = "Pantau Covid"
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
