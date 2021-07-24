a3c021e525074bbf4de1f23f19138a25640161e5
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 14:36:16 +0700

    Menambah method untuk hospital service

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/RemoteDataSource.kt
@@ -1,9 +1,13 @@
 package com.purwasadr.pantaucovid.data.source.remote
 
+import com.purwasadr.pantaucovid.data.source.local.entity.ProvinceHospitalEntity
 import com.purwasadr.pantaucovid.data.source.remote.network.ApiResponse
 import com.purwasadr.pantaucovid.data.source.remote.network.ApiService
+import com.purwasadr.pantaucovid.data.source.remote.network.HospitalService
+import com.purwasadr.pantaucovid.data.source.remote.response.CityResponse
 import com.purwasadr.pantaucovid.data.source.remote.response.CovidDataResponse
 import com.purwasadr.pantaucovid.data.source.remote.response.CovidResponse
+import com.purwasadr.pantaucovid.data.source.remote.response.ProvinceResponse
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -12,6 +16,7 @@ import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val apiService: ApiService,
+    private val hospitalService: HospitalService,
     private val defaultDispatchers: CoroutineDispatcher
 ) : IRemoteDataSource {
 
@@ -23,4 +28,24 @@ class RemoteDataSource @Inject constructor(
             emit(ApiResponse.Error(e.toString()))
         }
     }.flowOn(defaultDispatchers)
+
+    override fun getProvince(): Flow<ApiResponse<ProvinceResponse>> = flow {
+        try {
+            val results = hospitalService.getProvince()
+            emit(ApiResponse.Success(results))
+        } catch (e: Exception) {
+            emit(ApiResponse.Error(e.toString()))
+        }
+    }.flowOn(defaultDispatchers)
+
+    override fun getCities(id: String): Flow<ApiResponse<CityResponse>> = flow {
+        try {
+            val results = hospitalService.getCities(id)
+            emit(ApiResponse.Success(results))
+        } catch (e: Exception) {
+            emit(ApiResponse.Error(e.toString()))
+        }
+    }.flowOn(defaultDispatchers)
+
+
 }
