02e465dfeffe14890bcbf9bccf281d8cba43bf82
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 14:34:45 +0700

    Memberi extension funtion untuk mapping

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/ProvinceResponse.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/ProvinceResponse.kt
@@ -1,9 +1,14 @@
 package com.purwasadr.pantaucovid.data.source.remote.response
 
 import com.google.gson.annotations.SerializedName
+import com.purwasadr.pantaucovid.data.source.local.entity.ProvinceHospitalEntity
 
 data class ProvinceResponse(
 
 	@field:SerializedName("provinces")
-	val provinces: List<ProvincesItemResponse?>? = null
+	val provinces: List<ProvincesItemResponse>? = null
 )
+
+fun ProvinceResponse.toEntity() = this.provinces?.map {
+	ProvinceHospitalEntity(it.id, it.name)
+}
--- app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/ProvincesItemResponse.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/ProvincesItemResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class ProvincesItemResponse(
 
 	@field:SerializedName("name")
-	val name: String? = null,
+	val name: String,
 
 	@field:SerializedName("id")
 	val id: String
