492f25943ed3599acd8ef1b56311357b71f0fda7
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 16:14:13 +0700

    Menambahkan fungsi yang diperlukan

--- app/src/main/java/com/purwasadr/pantaucovid/ui/main/MainViewModel.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/ui/main/MainViewModel.kt
@@ -2,8 +2,13 @@ package com.purwasadr.pantaucovid.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.purwasadr.pantaucovid.data.IAppRepository
+import com.purwasadr.pantaucovid.data.Resource
+import com.purwasadr.pantaucovid.data.source.local.entity.CityEntity
+import com.purwasadr.pantaucovid.data.source.local.entity.HospitalEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +16,20 @@ class MainViewModel @Inject constructor(
     private val repository: IAppRepository
 ) : ViewModel() {
     val getCovidData = repository.getCovidData().asLiveData()
+
+    val province = repository.getProvinceList().asLiveData()
+
+    var cities: Flow<Resource<List<CityEntity>>>? = null
+
+    var hospitals: Flow<Resource<List<HospitalEntity>>>? = null
+
+    fun getCities(id: String): Flow<Resource<List<CityEntity>>> {
+
+        return repository.getCities(id)
+    }
+
+    fun getHospitals(provinceId: String, cityId: String): Flow<Resource<List<HospitalEntity>>> {
+
+        return  repository.getHospitals(provinceId, cityId)
+    }
 }
