5b05dfd64cef781669a64e99ae5639451526bc18
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 16:07:16 +0700

    Menambah method

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/local/ILocalDataSource.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/local/ILocalDataSource.kt
@@ -1,10 +1,20 @@
 package com.purwasadr.pantaucovid.data.source.local
 
+import com.purwasadr.pantaucovid.data.source.local.entity.CityEntity
 import com.purwasadr.pantaucovid.data.source.local.entity.CovidEntity
+import com.purwasadr.pantaucovid.data.source.local.entity.ProvinceHospitalEntity
 import kotlinx.coroutines.flow.Flow
 
 interface ILocalDataSource {
     fun getCovidData(): Flow<CovidEntity?>
 
     suspend fun insert(covidEntity: CovidEntity)
+
+    fun getProvinceList(): Flow<List<ProvinceHospitalEntity>>
+
+    suspend fun insertProvince(entity: List<ProvinceHospitalEntity>)
+
+    fun getCities(): Flow<List<CityEntity>>
+
+    suspend fun insertCities(entity: List<CityEntity>)
 }
