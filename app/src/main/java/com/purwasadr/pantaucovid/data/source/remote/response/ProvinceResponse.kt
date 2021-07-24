2c423ed83b5538cb0a4230b3db4957af911cc5e8
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 12:58:58 +0700

    Menambah class response untuk hospital service

---
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/ProvinceResponse.kt
@@ -0,0 +1,9 @@
+package com.purwasadr.pantaucovid.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ProvinceResponse(
+
+	@field:SerializedName("provinces")
+	val provinces: List<ProvincesItemResponse?>? = null
+)
