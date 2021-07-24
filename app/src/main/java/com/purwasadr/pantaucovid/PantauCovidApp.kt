522b3321cd3abd396d2f42820cef6c56b8dcfc3f
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 11:05:24 +0700

    Membuat Application untuk mengatur Hilt dan Timber

---
+++ app/src/main/java/com/purwasadr/pantaucovid/PantauCovidApp.kt
@@ -0,0 +1,17 @@
+package com.purwasadr.pantaucovid
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PantauCovidApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
