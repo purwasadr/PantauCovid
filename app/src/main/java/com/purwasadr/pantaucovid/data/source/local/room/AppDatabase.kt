1f12e83c3a2403d8318db1134b38f5c3b6a2637c
Purwa Sadr <purwasadr@gmail.com>
2021-07-24 16:06:26 +0700

    Menambah dao dan version

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/local/room/AppDatabase.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/local/room/AppDatabase.kt
@@ -2,15 +2,27 @@ package com.purwasadr.pantaucovid.data.source.local.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import com.purwasadr.pantaucovid.data.source.local.entity.CityEntity
 import com.purwasadr.pantaucovid.data.source.local.entity.CovidEntity
+import com.purwasadr.pantaucovid.data.source.local.entity.HospitalEntity
+import com.purwasadr.pantaucovid.data.source.local.entity.ProvinceHospitalEntity
 
 @Database(
-    entities = [CovidEntity::class],
-    version = 1,
+    entities = [
+        CovidEntity::class,
+        ProvinceHospitalEntity::class,
+        CityEntity::class,
+        HospitalEntity::class
+    ],
+    version = 4,
     exportSchema = false
 )
-@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun covidDao(): CovidDao
+
+    abstract fun provinceDao(): ProvinceHospitalDao
+
+    abstract fun cityDao(): CityDao
+
+    abstract fun hospitalDao(): HospitalDao
 }
