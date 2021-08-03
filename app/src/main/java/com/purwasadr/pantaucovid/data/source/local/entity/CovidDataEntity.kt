a2dfac5fa893632ad2a9eace458457f156bd7557
Purwa Sadr <purwasadr@gmail.com>
2021-08-03 16:54:02 +0700

    Add Entity for covid rate

---
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/local/entity/CovidDataEntity.kt
@@ -0,0 +1,24 @@
+package com.purwasadr.pantaucovid.data.source.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "covid_data")
+data class CovidDataEntity(
+    @PrimaryKey
+    val id: Long = 0,
+    val dataJumlahOdp: Long? = null,
+    val dataJumlahPdp: Long? = null,
+    val dataTotalSpesimen: Long? = null,
+    val dataTotalSpesimenNegatif: Long? = null,
+    val penambahanCreated: String? = null,
+    val penambahanJumlahMeninggal: Long? = null,
+    val penambahanTanggal: String? = null,
+    val penambahanJumlahSembuh: Long? = null,
+    val penambahanJumlahPositif: Long? = null,
+    val penambahanJumlahDirawat: Long? = null,
+    val totalJumlahMeninggal: Long? = null,
+    val totalJumlahSembuh: Long? = null,
+    val totalJumlahPositif: Long? = null,
+    val totalJumlahDirawat: Long? = null
+)
