6b6b7cb9473c953dec47cafa431457aca1aff4b9
Purwa Sadr <purwasadr@gmail.com>
2021-08-04 15:02:05 +0700

    Add mapper interface

---
+++ app/src/main/java/com/purwasadr/pantaucovid/data/mapper/Mapper.kt
@@ -0,0 +1,9 @@
+package com.purwasadr.pantaucovid.data.mapper
+
+interface Mapper<F, T> {
+    suspend fun map(from: F): T
+}
+
+interface IndexedMapper<F, T> {
+    suspend fun map(index: Int, from: F): T
+}
