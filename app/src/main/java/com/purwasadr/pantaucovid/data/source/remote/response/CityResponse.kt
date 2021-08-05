bcf8b49e32f98f13c8f27a93a6c391716498e645
Purwa Sadr <purwasadr@gmail.com>
2021-08-05 14:53:27 +0700

    Change response type

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/CityResponse.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/remote/response/CityResponse.kt
@@ -1,14 +1,9 @@
 package com.purwasadr.pantaucovid.data.source.remote.response
 
 import com.google.gson.annotations.SerializedName
-import com.purwasadr.pantaucovid.data.source.local.entity.CityEntity
 
 data class CityResponse(
 
 	@field:SerializedName("cities")
-	val cities: List<CitiesItemResponse>? = null
+	val cities: List<CityItemResponse>? = null
 )
-
-fun CityResponse.toEntity() = this.cities?.map {
-	CityEntity(it.id, it.name)
-}
