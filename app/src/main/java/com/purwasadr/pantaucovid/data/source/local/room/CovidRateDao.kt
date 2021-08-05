39ca7507664b87b22e7e6c8b62e6c6dbef890cbd
Purwa Sadr <purwasadr@gmail.com>
2021-08-05 14:50:13 +0700

    change model name and table name

--- app/src/main/java/com/purwasadr/pantaucovid/data/source/local/room/CovidRateDao.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/data/source/local/room/CovidRateDao.kt
@@ -2,11 +2,11 @@ package com.purwasadr.pantaucovid.data.source.local.room
 
 import androidx.room.Dao
 import androidx.room.Query
-import com.purwasadr.pantaucovid.data.source.local.entity.CovidDataEntity
+import com.purwasadr.pantaucovid.data.source.local.entity.CovidRateEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-abstract class CovidRateDao : BaseDao<CovidDataEntity> {
-    @Query("SELECT * FROM covid_data WHERE id = 0")
-    abstract fun observeEntity(): Flow<CovidDataEntity>
+abstract class CovidRateDao : BaseDao<CovidRateEntity> {
+    @Query("SELECT * FROM covid_rate WHERE id = 0")
+    abstract fun observeEntity(): Flow<CovidRateEntity>
 }
