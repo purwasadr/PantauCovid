22ff789edc02431f4e5c85d85fb3efeff37672d9
Purwa Sadr <purwasadr@gmail.com>
2021-08-13 17:45:01 +0700

    delete unused entity

--- app/src/main/java/com/purwasadr/pantaucovid/model/Covid.kt
+++
@@ -1,9 +0,0 @@
-package com.purwasadr.pantaucovid.model
-
-data class Covid(
-    val jumlahPositif: Int? = null,
-    val jumlahSembuh: Int? = null,
-    val jumlahMeninggal: Int? = null,
-    val jumlahOdp: Int?,
-    val jumlahPdp: Int?,
-)
