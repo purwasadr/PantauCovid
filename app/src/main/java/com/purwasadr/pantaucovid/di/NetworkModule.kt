2afe625737d083c317c89046142ec90e6b3ec9c2
Purwa Sadr <purwasadr@gmail.com>
2021-08-13 17:41:50 +0700

    define separate okHttpClient from apiservice

--- app/src/main/java/com/purwasadr/pantaucovid/di/NetworkModule.kt
+++ app/src/main/java/com/purwasadr/pantaucovid/di/NetworkModule.kt
@@ -1,12 +1,16 @@
 package com.purwasadr.pantaucovid.di
 
-import com.purwasadr.pantaucovid.data.source.remote.network.ApiService
+import com.purwasadr.pantaucovid.BuildConfig
+import com.purwasadr.pantaucovid.data.source.remote.network.CovidRateService
 import com.purwasadr.pantaucovid.data.source.remote.network.HospitalService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
+import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,9 +18,28 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideApiService(): ApiService = ApiService.create()
+    fun provideOkHttpClient() =
+        OkHttpClient
+            .Builder()
+            .also {
+                if (BuildConfig.DEBUG) {
+                    it.addInterceptor(
+                        HttpLoggingInterceptor()
+                            .setLevel(HttpLoggingInterceptor.Level.BODY)
+                    )
+                }
+            }
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
 
     @Singleton
     @Provides
-    fun provideHospital(): HospitalService = HospitalService.create()
+    fun provideApiService(client: OkHttpClient): CovidRateService =
+        CovidRateService.create(client)
+
+    @Singleton
+    @Provides
+    fun provideHospital(client: OkHttpClient): HospitalService =
+        HospitalService.create(client)
 }
