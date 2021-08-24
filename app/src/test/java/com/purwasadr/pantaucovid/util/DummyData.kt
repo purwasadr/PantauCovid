acd31d072c22b5d37d4ed8bc0842331e4c416b3e
Purwa Sadr <purwasadr@gmail.com>
2021-08-24 10:57:37 +0700

    add util for test

---
+++ app/src/test/java/com/purwasadr/pantaucovid/util/DummyData.kt
@@ -0,0 +1,11 @@
+package com.purwasadr.pantaucovid.util
+
+import com.purwasadr.pantaucovid.model.City
+
+object DummyData {
+    val cityDomains = listOf(
+        City(id = "fuyuk", name = "Fuyuki"),
+        City(id = "tozuro", name = "Kartasura"),
+        City(id = "suko", name = "Sukoharjo")
+    )
+}
