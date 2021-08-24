04b9a583c8979616492901ae2116908c0b3639d8
Purwa Sadr <purwasadr@gmail.com>
2021-08-24 20:13:44 +0700

    Add class and manifest for testing with hilt

---
+++ app/src/debug/java/com/purwasadr/pantaucovid/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.purwasadr.pantaucovid
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity() {
+}
